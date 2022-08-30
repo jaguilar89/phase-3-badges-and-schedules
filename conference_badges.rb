66cee1a11e3805782f49bdb86eae8f86b9dcf432
Jose Aguilar <jaguilarny@gmail.com>
2022-08-29 23:46:53 -0400

    commit tree 3d5c60bb707e6d970846d6c752e9b8b549395b68

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker (name)
+     "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (speaker_list)
+    speaker_list.map {|speaker| "Hello, my name is #{speaker}."}
+end
+
+def assign_rooms (speaker_list)
+    speaker_list.map
+                .with_index(1) do |speaker, index|
+                    "Hello, #{speaker}! You'll be assigned to room #{index}!"
+                end
+end
+
+def printer (speaker_list)
+    batch_badge_creator(speaker_list).each do |badge|
+        puts badge
+    end
+    assign_rooms(speaker_list).each do |assignment|
+        puts assignment
+    end
+end
